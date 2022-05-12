5dea3675f8ef65fb872795af45ca34545961626e
Denny Maverick <den_maverick177@mail.ru>
2022-05-12 12:49:52 +0300

    Adding the tabs

--- js/script.js
+++ js/script.js
@@ -1,8 +1,25 @@
-import burgerMenu from "./burger.js";
+const burger = document.querySelector(".burger");
+const menu = document.querySelector(".navigation__list");
+burger.addEventListener("click", () => {
+  burger.classList.toggle("burger--active");
+  menu.classList.toggle("navigation__list--active");
+});
 
-burgerMenu(
-  '.burger',
-  '.navigation__list',
-  'burger--active',
-  'navigation__list--active'
-)
+const tabsBtns = document.querySelectorAll(".tabs__btn");
+
+tabsBtns.forEach((btn) => {
+  btn.addEventListener("click", function (event) {
+    const tabsContents = document.querySelectorAll(".tabs__item");
+    tabsBtns.forEach((btn) => {
+      btn.classList.remove("tabs__btn--active");
+    });
+    const currentContentId = "#" + event.target.dataset.type;
+
+    const currentContent = document.querySelector(currentContentId);
+    tabsContents.forEach((content) => {
+      content.classList.remove("tabs__item--active");
+    });
+    currentContent.classList.add("tabs__item--active");
+    this.classList.add("tabs__btn--active");
+  });
+});
